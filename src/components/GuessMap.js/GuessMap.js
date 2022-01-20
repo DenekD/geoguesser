2abc5203bef57e674b504a708dc08a424ac2b05a
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-01-20 10:03:56 +0100

    refactoring

--- src/components/GuessMap.js/GuessMap.js
+++ src/components/GuessMap.js/GuessMap.js
@@ -19,24 +19,6 @@ const libraries = ["places"];
 // zIndex: "10",
 // };
 
-const svgIcon = (
-  <svg
-    aria-hidden="true"
-    focusable="false"
-    data-prefix="fas"
-    data-icon="map-pin"
-    class="svg-inline--fa fa-map-pin fa-w-9"
-    role="img"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 288 512"
-  >
-    <path
-      fill="#8C3E85"
-      d="M112 316.94v156.69l22.02 33.02c4.75 7.12 15.22 7.12 19.97 0L176 473.63V316.94c-10.39 1.92-21.06 3.06-32 3.06s-21.61-1.14-32-3.06zM144 0C64.47 0 0 64.47 0 144s64.47 144 144 144 144-64.47 144-144S223.53 0 144 0zm0 76c-37.5 0-68 30.5-68 68 0 6.62-5.38 12-12 12s-12-5.38-12-12c0-50.73 41.28-92 92-92 6.62 0 12 5.38 12 12s-5.38 12-12 12z"
-    ></path>
-  </svg>
-);
-
 const mapOptions = {
   styles: mapstyles,
   disableDefaultUI: false,
@@ -159,16 +141,6 @@ const GuessMap = () => {
     history.push("gameResult");
   };
 
-  // const nextMapHandler = async () => {
-  //   const locations = await randomStreetView.getRandomLocations(1);
-  //   dispatch({
-  //     type: "SET_STREET_VIEW_COORDS",
-  //     coords: { lat: locations[0][0], lng: locations[0][1] },
-  //   });
-  //   dispatch({ type: "TOGGLE_STREET_VIEW_MARKER_VISIBILITY" });
-  //   mapRef.current.panTo({ lat: locations[0][0], lng: locations[0][1] });
-  // };
-
   if (loadError) return "error loading map";
   if (!isLoaded) return "loading map";
 
@@ -212,7 +184,6 @@ const GuessMap = () => {
         <Marker
           onLoad={onLoad}
           position={markerCoords}
-          // icon={svgIcon}
           icon={{
             url: "./map-pin-solid.svg",
             origin: new window.google.maps.Point(0, 0),
